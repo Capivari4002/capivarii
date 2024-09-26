96aac7dba16146a2e6fd997cb09bf6f705062910
Capivari4002 <teixeira.rodrigues@escola.pr.gov.br>
2024-09-26 12:10:13 -0300

    serrt

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 alert('rafaela.31 e juliane.18')
-let nome=parseInt (prompt('acrescente um numero'))
+let numero=parseInt (prompt('acrescente um numero'))
 function soma(numero1,numero2 )
 {return numero1+numero2;}
 console.log (soma(1,2));
 function areaQuadrado( lado )
 {return lado**2}
-console.log (areaQuadrado(6));
+console.log (areaQuadrado(numero));
+let media=function()
+{;}
+document write();
