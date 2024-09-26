27f6f5fdb3ecc3dac0153eb1e5e7e3d4f6b57e06
Capivari4002 <teixeira.rodrigues@escola.pr.gov.br>
2024-09-26 12:20:33 -0300

    fh

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 alert('rafaela.31 e juliane.18')
-let numero=parseInt (prompt('acrescente um numero'))
+let 1=parseInt (prompt('qual sua 1 nota'))
+let 2=parseInt (prompt('qual sua 2 nota'))
+let 3=parseInt (prompt('qual sua 3 nota'))
+
 function soma(numero1,numero2 )
 {return numero1+numero2;}
 console.log (soma(1,2));
 function areaQuadrado( lado )
 {return lado**2}
 console.log (areaQuadrado(numero));
-let media=function()
-{return ;}
+let media=function(1,2,3)
+{return 1+2+3}
 document write();
