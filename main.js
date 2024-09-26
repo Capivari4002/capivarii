60eb41adf69ee3fb5869b6b2aae96b8ed59a62d0
Capivari4002 <teixeira.rodrigues@escola.pr.gov.br>
2024-09-26 11:35:19 -0300

    def

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ alert('rafaela.31 e juliane.18')
 function soma(numero1,numero2 )
 {return numero1+numero2;}
 console.log (soma(1,2));
+function potenciaçao(numero1,numero2)
+{return numero1**numero2}
+console.log (potenciaçao(1,2));
