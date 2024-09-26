1763c3d8e8600027fe90a43c2e5281c2bbb14549
Capivari4002 <teixeira.rodrigues@escola.pr.gov.br>
2024-09-26 12:51:00 -0300

    Update main.js
    Bbd

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 alert('rafaela.31 e juliane.18')
-let 1=parseInt (prompt('qual sua 1 nota'))
-let 2=parseInt (prompt('qual sua 2 nota'))
-let 3=parseInt (prompt('qual sua 3 nota'))
+let número= parseInt(prompt('acrecente um número'))
+let primeiro=parseInt (prompt('qual sua 1 nota'));
+let segundo=parseInt (prompt('qual sua 2 nota'));
+let terceiro=parseInt (prompt('qual sua 3 nota'));
 
 function soma(numero1,numero2 )
 {return numero1+numero2;}
@@ -9,6 +10,6 @@ console.log (soma(1,2));
 function areaQuadrado( lado )
 {return lado**2}
 console.log (areaQuadrado(numero));
-let media=function(1,2,3)
-{return 1+2+3}
-document write();
+let media=function(primero, segundo, terceiro)
+{return (primeiro+segundo+terceiro)/3}
+document.withe(media(primeiro,segundo, terceiro))
