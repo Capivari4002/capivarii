697b95ec6de5a66dd966bd2f8c24becf9b7d548d
Capivari4002 <teixeira.rodrigues@escola.pr.gov.br>
2024-10-03 11:27:10 -0300

    v

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
-alert('rafaela.31 e juliane.18')
-let número= parseInt(prompt('acrecente um número'))
-let primeiro=parseInt (prompt('qual sua 1 nota'));
-let segundo=parseInt (prompt('qual sua 2 nota'));
-let terceiro=parseInt (prompt('qual sua 3 nota'));
+// alert('rafaela.31 e juliane.18')
+// let número= parseInt(prompt('acrecente um número'))
+// let primeiro=parseInt (prompt('qual sua 1 nota'));
+// let segundo=parseInt (prompt('qual sua 2 nota'));
+// let terceiro=parseInt (prompt('qual sua 3 nota'));
 
-function soma(numero1, numero2) {return numero1 + numero2; }
-console.log(soma(1, 2));
-function areaQuadrado(lado) {return lado**2; }
-console.log(areaQuadrado('numero'));
-let media=function (primero, segundo, terceiro){
-    return (primeiro + segundo + terceiro)/3
-}
+// function soma(numero1, numero2) {return numero1 + numero2; }
+// console.log(soma(1, 2));
+// function areaQuadrado(lado) {return lado**2; }
+// console.log(areaQuadrado('numero'));
+// let media=function (primero, segundo, terceiro){
+//     return (primeiro + segundo + terceiro)/3
+// }
 
-document.write(media(primeiro,segundo, terceiro))
+// document.write(media(primeiro,segundo, terceiro))
+
+let estados{
+'São Paulo',
+'rio de janeiro',
+'rio grande do sul' ,
+'minas Gerais' ,
+'santa catarina' ,
+'parana' ,
+'distrito federal' ,
+'ceara' ,
+'espirito santo',
+'goias' ,
+'rio grande do norte' ,
+'pernambuco' ,
+'bahia' ,
+'sergipe' ,
+'paraiba' ,
+'para' ,
+'amazonas',
+'mato grosso do sul' ,
+'rondonia' ,
+'mato grosso' ,
+'piaui' ,
+'maranhao' ,
+'tocantins' ,
+'acre' ,
+'alagoas',
+'roraima' ,
+'amapa' }
